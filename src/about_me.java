4530bd0014e6bc60586c7b8f27f7efee004eca79
Michael Balling <mballing@students.mchenry.edu>
2018-01-21 18:54:26 -0600

    Initial commit

---
+++ src/about_me.java
@@ -0,0 +1,8 @@
+public class about_me {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Mike Balling.");
+        System.out.println("I am 30 years old, married, with our first baby on the way!");
+        System.out.println("My career as a pilot has taken me all around the country, and soon the world.");
+        System.out.println("Programming allows me to diversify my skill set while tinkering with technology.");
+    }
+}
